c80805c0b67cc25eea5e240ecc6a371a25060bfd
Theo Sementa <theosementa@gmail.com>
2025-08-24 20:00:21 +0200

    first commit

---
+++ DesignPatterns-swift/DesignPatterns_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatterns_swiftApp.swift
+//  DesignPatterns-swift
+//
+//  Created by Theo Sementa on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatterns_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DesignPatterns-swiftTests/DesignPatterns_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatterns_swiftTests.swift
+//  DesignPatterns-swiftTests
+//
+//  Created by Theo Sementa on 24/08/2025.
+//
+
+import Testing
+@testable import DesignPatterns_swift
+
+struct DesignPatterns_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
